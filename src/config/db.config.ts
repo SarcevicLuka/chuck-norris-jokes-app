e21e8adf1d6a7cf135ef09b96af4b7c22e4f8238
SarcevicLuka <luka.sarac99@gmail.com>
2023-06-26 20:01:44 +0200

    Removed tests

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,13 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const dbUrl = process.env.POSTGRES_DB_URL as string;
+const dbUrl =
+	process.env.POSTGRES_DB_URL ||
+	"postgres://postgres:profico@localhost:5432/postgres";
 
-// Create new Sequelize instance with database url and pool setup
 export const sequelize: Sequelize = new Sequelize(dbUrl, {
 	pool: {
 		max: 10,
 		min: 0,
 		acquire: 5000,
-		idle: 0
+		idle: 1000
 	}
 });
